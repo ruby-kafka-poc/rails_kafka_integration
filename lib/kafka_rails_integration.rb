8ed5801d619b9cfe64370bb775ba7f4a0794f867
mberrueta <matiasberrueta@gmail.com>
2022-05-06 06:21:41 -0300

    First commit 🚀

---
+++ lib/kafka_rails_integration.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'kafka_rails_integration/version'
+
+module KafkaRailsIntegration
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/kafka_rails_integration_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe KafkaRailsIntegration do
+  it 'has a version number' do
+    expect(KafkaRailsIntegration::VERSION).not_to be_nil
+  end
+end

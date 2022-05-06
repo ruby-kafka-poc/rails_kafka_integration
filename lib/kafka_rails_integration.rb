c293949b04871a10b830b614ffeeb383b4a118ed
mberrueta <matiasberrueta@gmail.com>
2022-05-06 11:19:16 -0300

    require

--- lib/kafka_rails_integration.rb
+++ lib/kafka_rails_integration.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'kafka_rails_integration/concerns/model/changes_trackeable'
+require_relative 'kafka_rails_integration/concerns/model/eventeable'
 require_relative 'kafka_rails_integration/middlewares/deliver_messages'
 require_relative 'kafka_rails_integration/producer/producer'
 require_relative 'kafka_rails_integration/version'

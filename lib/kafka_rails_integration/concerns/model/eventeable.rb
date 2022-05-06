10f08745a9516da009dfd6a89394221a6a1f3453
mberrueta <matiasberrueta@gmail.com>
2022-05-06 10:25:20 -0300

    fix libraries

--- lib/kafka_rails_integration/concerns/model/eventeable.rb
+++ lib/kafka_rails_integration/concerns/model/eventeable.rb
@@ -1,37 +1,43 @@
 # frozen_string_literal: true
 
-module Eventeable
-  extend ActiveSupport::Concern
+module KafkaRailsIntegration
+  module Concerns
+    module Model
+      module Eventeable
+        extend ActiveSupport::Concern
 
-  included do
-    after_commit :publish_created!, on: :create
-    after_commit :publish_edited!, on: :update
-    after_commit :publish_deleted!, on: :destroy
-  end
+        included do
+          after_commit :publish_created!, on: :create
+          after_commit :publish_edited!, on: :update
+          after_commit :publish_deleted!, on: :destroy
+        end
 
-  private
+        private
 
-  def publish!(action)
-    Kafka::Producer.produce(payload(action), "#{self.class.name}-#{action}".underscore)
-  end
+        def publish!(action)
+          Kafka::Producer.produce(payload(action), "#{self.class.name}-#{action}".underscore)
+        end
 
-  def publish_created!
-    publish!(:created)
-  end
+        def publish_created!
+          publish!(:created)
+        end
 
-  def publish_edited!
-    publish!(:edited)
-  end
+        def publish_edited!
+          publish!(:edited)
+        end
 
-  def publish_deleted!
-    publish!(:deleted)
-  end
+        def publish_deleted!
+          publish!(:deleted)
+        end
 
-  def payload(action)
-    {
-      entity: self.class.name,
-      object: as_json,
-      action:
-    }
+        def payload(action)
+          {
+            entity: self.class.name,
+            object: as_json,
+            action:
+          }
+        end
+      end
+    end
   end
 end

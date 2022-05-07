dce567881e5a473be1fa3bdb044d836fa56f470a
mberrueta <matiasberrueta@gmail.com>
2022-05-07 09:00:40 -0300

    test requires

--- lib/kafka_rails_integration.rb
+++ lib/kafka_rails_integration.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require_relative 'kafka_rails_integration/concerns/model/changes_trackeable'
-require_relative 'kafka_rails_integration/concerns/model/eventeable'
-require_relative 'kafka_rails_integration/middlewares/deliver_messages'
-require_relative 'kafka_rails_integration/producer/producer'
-require_relative 'kafka_rails_integration/version'
+require 'kafka_rails_integration/concerns/model/changes_trackeable'
+require 'kafka_rails_integration/concerns/model/eventeable'
+require 'kafka_rails_integration/middlewares/deliver_messages'
+require 'kafka_rails_integration/producer/producer'
+require 'kafka_rails_integration/version'
 
 module KafkaRailsIntegration
   class Error < StandardError; end
